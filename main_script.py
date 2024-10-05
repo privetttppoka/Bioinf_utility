cce236e74e082e8a4e22dd1fe458b1b760bc9cec
privetttppoka <nazarova.al000@gmail.com>
2024-10-05 14:56:19 +0300

    Update filter_fastq and flake8

--- main_script.py
+++ main_script.py
@@ -8,6 +8,17 @@ def filter_fastq(
     length_bounds: Union[tuple[int, int], int] = (0, 2**32),
     quality_threshold: float = 0,
 ) -> dict[str, tuple[str, str]]:
+    """The filter_fastq function filters fastq
+
+    by CG-composition, length, and reading quality level.
+    All these parameters can be changed by passing
+    the necessary values to the function argument:
+    gc_bounds(tuple),length_bounds(tuple), quality_threshold(float).
+    """
+    if isinstance(gc_bounds, float) or isinstance(gc_bounds, int):
+        gc_bounds = (0, gc_bounds)
+    if isinstance(length_bounds, float) or isinstance(length_bounds, int):
+        length_bounds = (0, length_bounds)
 
     quality_summ = []
     final_dict = {}
@@ -16,12 +27,11 @@ def filter_fastq(
 
         for char in quality:
             quality_summ.append(ord(char) - 33)
-
         cg_content = (sequence.count("C") + sequence.count("G")) * 100 / len(sequence)
 
         if (
-            (length_bounds[0] <= len(sequence) <= length_bounds[1])
-            and (gc_bounds[0] <= cg_content <= gc_bounds[1])
+            (length_bounds[0] < len(sequence) <= length_bounds[1])
+            and (gc_bounds[0] < cg_content <= gc_bounds[1])
             and ((sum(quality_summ)) / len(quality_summ)) >= quality_threshold
         ):
             final_dict[name] = (sequence, quality)
@@ -30,6 +40,15 @@ def filter_fastq(
 
 
 def run_dna_rna_tools(*args: Union[str, list[str]]) -> Union[str, list[str]]:
+    """The run_dna_rna_tools function can perform basic bioinformatic operations:
+
+    transcribe, reverse_complement, reverse, complement.
+    This function also checks for DNA or RNA sequences
+    otherwise the sequence is not processed.
+    Sequences are taken as input and the last argument
+    is one of the operations to be performed on them:
+    transcribe, reverse_complement, reverse, complement.
+    """
 
     option = args[-1]
     seqs = list(args[:-1])
@@ -55,6 +74,3 @@ def run_dna_rna_tools(*args: Union[str, list[str]]) -> Union[str, list[str]]:
     if len(result) == 1:
         return result[0]
     return result
-
-
-(run_dna_rna_tools("aaaUUUUuuuGGGCCC", "complement"))
